36697bc22e714340e2827d2d7869f32a9749c2fd
Rangkynsai Saio <47221516+Rangkynsai@users.noreply.github.com>
2019-08-12 22:41:25 +0530

    Add files via upload

---
+++ tic_tac_toe.py
@@ -0,0 +1,140 @@
+# ---Global variable---
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-"]
+game_still_going = True
+
+winner = None
+
+current_player = "X"
+
+
+def display():
+    print(board[0] + "|" + board[1] + "|" + board[2])
+    print(board[3] + "|" + board[4] + "|" + board[5])
+    print(board[6] + "|" + board[7] + "|" + board[8])
+
+
+def play_game():
+    display()
+    while game_still_going:
+        handle_turn(current_player)
+
+        check_if_game_over()
+
+        flip_player()
+    if winner == "X" or winner == "O":
+        print(winner + " Won ")
+    elif winner == None:
+        print("Tie")
+
+
+def handle_turn(player):
+    print(player + " turn.")
+    position = input("Choose a position from 1 to 9: ")
+    valid = False
+    while not valid:
+        while position not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+            position = input("Choose a position from 1 to 9: ")
+        position = int(position) - 1
+        if board[position] == "-":
+            valid = True
+        else:
+            print("You can't go there, Go again")
+    board[position] = player
+    display()
+
+
+def check_if_game_over():
+    check_for_winner()
+    check_if_tie()
+
+
+def check_for_winner():
+    global winner
+    # check Row
+    row_winner = check_row()
+    # check colum
+    column_winner = check_column()
+    # check diagonal
+    diagonal_winner = check_diagonal()
+    if row_winner:
+        winner = row_winner
+    elif column_winner:
+        winner = column_winner
+    elif diagonal_winner:
+        winner = diagonal_winner
+    else:
+        winner = None
+
+
+def check_row():
+    global game_still_going
+
+    row_1 = board[0] == board[1] == board[2] != "-"
+    row_2 = board[3] == board[4] == board[5] != "-"
+    row_3 = board[6] == board[7] == board[8] != "-"
+
+    if row_1 or row_2 or row_3:
+        game_still_going = False
+    if row_1:
+        return board[0]
+    elif row_2:
+        return board[3]
+    elif row_3:
+        return board[6]
+    else:
+        return None
+
+
+def check_column():
+    global game_still_going
+
+    column_1 = board[0] == board[3] == board[6] != "-"
+    column_2 = board[1] == board[4] == board[7] != "-"
+    column_3 = board[2] == board[5] == board[8] != "-"
+
+    if column_1 or column_2 or column_3:
+        game_still_going = False
+    if column_1:
+        return board[0]
+    elif column_2:
+        return board[1]
+    elif column_3:
+        return board[2]
+    else:
+        return
+
+
+def check_diagonal():
+    global game_still_going
+
+    digonal_1 = board[0] == board[4] == board[8] != "-"
+    digonal_2 = board[2] == board[4] == board[6] != "-"
+
+    if digonal_1 or digonal_2:
+        game_still_going = False
+    if digonal_1:
+        return board[0]
+    elif digonal_2:
+        return board[2]
+    else:
+        return None
+
+
+def check_if_tie():
+    global game_still_going
+    if "-" not in board:
+        game_still_going = False
+        return True
+    else:
+        return False
+
+
+def flip_player():
+    global current_player
+    if current_player == "X":
+        current_player = "O"
+    elif current_player == "O":
+        current_player = "X"
+play_game()
