27d218fd7dd00ada2808dbcfcfccbb0f85ed2351
Rangkynsai Saio <47221516+Rangkynsai@users.noreply.github.com>
2019-10-07 14:21:59 +0530

    numberOperation

---
+++ numberOp.py
@@ -0,0 +1,18 @@
+value1 = 100
+value2 = 200
+#addition
+print(value1+value2)
+#subtraction
+print(value1-value2)
+#multiplication
+print(value1*value2)
+#division
+print(value1/value2)
+#reaminder
+print(value1%value2)
+#division and round
+print(value1//value2)
+#exponential
+value1 = 3
+value2 = 3
+print(value1**value2)
