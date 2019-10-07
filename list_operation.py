7cccc40a18be09254ffc2220e74f0f9db52f366d
Rangkynsai Saio <47221516+Rangkynsai@users.noreply.github.com>
2019-10-07 15:25:31 +0530

    List Operation

--- list_operation.py
+++ list_operation.py
@@ -1,22 +1,20 @@
-n = int(input('Number of command: '))
-lst = []
-for i in range(n):
-    str1 = input('Command: ')
-    if str1 == 'insert':
-        position = int(input('Enter the position: '))
-        value =int(input('Value: '))
-        lst.insert(position,value)  
-    elif str1 == 'remove':
-        value =int(input('Value: '))
-        lst.remove(value)
-    elif str1 == 'append':
-        value =int(input('Value: '))
-        lst.append(value)
-    elif str1 == 'sort':
-        lst.sort()
-    elif str1 == 'pop':
-        lst.pop()
-    elif str1 == 'reverse':
-        lst.reverse()
-    elif str1 == 'print':
-        print(lst)
+#creating a list
+'''car_name = ['bmw','honda','ford','bugati','ducati','maruti','tesla']
+#iterating throught list
+for carname in car_name:
+    print(carname.title()+'\n')
+print('All Done!!!')
+squares = []
+for i in range(1,11):
+    square = i**2
+    squares.append(square)
+print(squares)'''
+#copying a list form one list to the other list using for loop
+old_list = ['a','b','c','d','f']
+new_list = []
+for lst in old_list:
+    new_list.append(lst)
+print(new_list)
+#finding maximum and minimum
+print(max(old_list))
+print(min(old_list))
