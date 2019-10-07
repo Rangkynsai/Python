c7e112045ca8e4b95ce6797335db9b8e2c43d737
Rangkynsai Saio <47221516+Rangkynsai@users.noreply.github.com>
2019-10-07 14:16:24 +0530

    string Operation

---
+++ strin.py
@@ -0,0 +1,12 @@
+message = 'One mine what happening'
+message1 = 'To the future world'
+#changing the first letter of each word into capital lletter.
+print(message.title())
+#changing the letter into capital letter
+print(message.upper())
+#changing the letter into small letter
+print(message.lower())
+#concatenating the two string
+print(message+' '+message1)
+#find the index of the character
+print(message.find('a'))
