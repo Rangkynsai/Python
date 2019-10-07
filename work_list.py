da29b6188d897e048af34ad6874025abbaeb76d9
Rangkynsai Saio <47221516+Rangkynsai@users.noreply.github.com>
2019-10-07 15:35:50 +0530

    iterating the list

---
+++ work_list.py
@@ -0,0 +1,8 @@
+magician = ['alice','david','alex','carolina']
+#looping through a list
+for magicians in magician:
+    print(magicians)
+#more with loop
+for magic in magician:
+    print(magic.title()+", that was a great trick.")
+print('Thank you, everyone. That was a great magic show.')
