c85a59aac26d253d258d35f0e2c45045eeae328e
Rangkynsai Saio <47221516+Rangkynsai@users.noreply.github.com>
2020-06-01 19:59:11 +0530

    practice

---
+++ fantasy.py
@@ -0,0 +1,10 @@
+def displayInventory(inventory):
+    print("Inventory: ")
+    total = 0
+    for k,v in inventory.items():
+        print(str(v)+" "+k)
+        total+=v
+    print(total)
+
+player = {'rope':1,'torch':6,'gold-coin':42,'dagger':1,'arrow':12}
+displayInventory(player)
