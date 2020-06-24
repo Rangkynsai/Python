c87534de42edf700892155ae99d934f34427a688
Rangkynsai Saio <47221516+Rangkynsai@users.noreply.github.com>
2020-06-24 20:05:13 +0530

    Update Tic_Tac_Toe Game.py

--- Tic_Tac_Toe Game.py
+++ Tic_Tac_Toe Game.py
@@ -1,8 +1,8 @@
 board = ["-","-","-",
-		"-","-","-"
-		,"-","-","-"]
+	 "-","-","-",
+	 "-","-","-"]
 game_ove = True
-
+#displaying the board
 def display_board(board):
 	print()
 	print(board[0]+"|"+board[1]+"|"+board[2])
@@ -23,36 +23,37 @@ def game_over(board,current_player):
 	global game_ove
 	if board[0] == "X" and board[1] == "X" and board[2] == "X" or (board[0] == "O" and board[1] == "O" and board[2] == "O"):
 		game_ove = False
-		print(current_player+" Won.")
+		print("Yay "+current_player+" Won!!!")
 		
 	elif board[3] == "X" and board[4] == "X" and board[5] == "X" or (board[3] == "O" and board[4] == "O" and board[5] == "O"):
 		game_ove = False
-		print(current_player+" Won.")
+		print("Yay "+current_player+" Won!!!")
 	elif board[6] == "X" and board[7] == "X" and board[8] == "X" or (board[6] == "O" and board[7] == "O" and board[8] == "O"):
 		game_ove = False
-		print(current_player+" Won.")
+		print("Yay "+current_player+" Won!!!")
 	#col
-	elif board[0] == "X" and board[3] == "X" and board[5] == "X" or (board[0] == "O" and board[3] == "O" and board[6] == "O"):
+	elif board[0] == "X" and board[3] == "X" and board[6] == "X" or (board[0] == "O" and board[3] == "O" and board[6] == "O"):
 		game_ove = False
-		print(current_player+" Won.")
+		print("Yay "+current_player+" Won!!!")
 	elif board[1] == "X" and board[4] == "X" and board[7] == "X" or (board[1] == "O" and board[4] == "O" and board[7] == "O"):
 		game_ove = False
-		print(current_player+" Won.")
+		print("Yay "+current_player+" Won!!!")
 	elif board[2] == "X" and board[5] == "X" and board[8] == "X" or (board[2] == "O" and board[5] == "O" and board[8] == "O"):
 		game_ove = False
-		print(current_player+" Won.")
+		print("Yay "+current_player+" Won!!!")
 	#diagonal
 	elif board[0] == "X" and board[4] == "X" and board[8] == "X" or (board[0] == "O" and board[4] == "O" and board[8] == "O"):
 		game_ove = False
-		print(current_player+" Won.")
+		print("Yay "+current_player+" Won!!!")
 	elif board[2] == "X" and board[4] == "X" and board[6] == "X" or (board[2] == "O" and board[4] == "O" and board[6] == "O"):
 		game_ove = False
-		print(current_player+" Won.")
+		print("Yay "+current_player+" Won!!!")
 
 #playing game
 while game_ove:
 	print(current_player+" Turn.")
 	pos = (int(input("Choose a number from 1 to 9 : "))-1)
+	#not to overide the position
 	if "-" not in board[pos]:
 		print("you cannot go in the same position!!")
 		pos = (int(input("Choose a number from 1 to 9 : "))-1)
@@ -62,7 +63,7 @@ while game_ove:
 	display_board(board)
 	game_over(board,current_player)
 	tie_game(board)
-	if game_ove == False:
+	if not game_ove:
 		break
 	if current_player == "X":
 		current_player = "O"
