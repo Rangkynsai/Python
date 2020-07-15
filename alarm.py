a97326750f768ffdbce88492e9ea138a73ac3920
Rangkynsai Saio <47221516+Rangkynsai@users.noreply.github.com>
2020-07-15 21:38:11 +0530

    Add files via upload

---
+++ alarm.py
@@ -0,0 +1,5 @@
+import time,webbrowser
+for i in range(4):
+    time.sleep(3600)#Work for 1 hour
+    webbrowser.open('https://www.youtube.com/watch?v=NuIAYHVeFYs')
+    time.sleep(900)#break for 15 minutes
